42701ad4289ada88856423db1ab8265e1756944d
Dhruv Bhuva <dhruv.bhuva@crestinfotech.co.in>
2025-04-23 13:13:02 +0530

    changes

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "https://b501-2402-a00-408-758c-579-2cae-7032-f755.ngrok-free.app",
-  baseURL: "http://localhost:7001", // Uncomment if using localhost
+  baseURL: import.meta.env.VITE_API_URL, // Uncomment if using localhost
 });
 
 API.interceptors.request.use((req) => {
