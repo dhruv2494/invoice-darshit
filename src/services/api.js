aa2e7f59b331d5ece7f9fb4de54b55898e127f5d
Dhruv Bhuva <dhruv.bhuva@crestinfotech.co.in>
2025-04-30 11:12:19 +0530

    deploy changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,32 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://b501-2402-a00-408-758c-579-2cae-7032-f755.ngrok-free.app",
-  baseURL: import.meta.env.VITE_API_URL, // Uncomment if using localhost
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
+// Request interceptor
 API.interceptors.request.use((req) => {
-  req.headers["ngrok-skip-browser-warning"] = 1; // Corrected line
+  req.headers["ngrok-skip-browser-warning"] = 1;
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
+
+API.interceptors.response.use(
+  (response) => {
+    if (response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default API;
